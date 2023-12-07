1e585a2e4b238a2c4f8c06b7a452029535b5ed87
kushagra819 <83858701+kushagra819@users.noreply.github.com>
2023-12-08 01:53:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Hey Urja,I exactly don&#39;t know what&#39;s going on but I&#39;m here to listen,<br/> If you need to talk about something. <br/> & btww How was your day?";
+  question.innerHTML = "Hey Urja,I exactly don&#39;t know what&#39;s going on but I&#39;m here to listen, <br/>If you need to talk about something. <br/> & btww How was your day?";
   gif.src =
     "https://media.giphy.com/media/FTGah7Mx3ss04PcasF/giphy.gif";
 });
